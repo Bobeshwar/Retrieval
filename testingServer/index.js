8c1ddf3e7972882af32dfde136f60ae54ebe8c1f
Bobeshwar <ampanrane@gmail.com>
2023-12-06 21:54:42 -0500

    completed server

--- testingServer/index.js
+++ testingServer/index.js
@@ -1,10 +1,41 @@
 const axios = require("axios");
 
-async function evaluateResult(input, relevantResults){
+async function evaluateResultSingleInput(input, relevantResult){
     response = await axios.post("http://localhost:8080/match", {
-        words: ["batman", "begins"],
+        query: input,
     })
-    console.log(response);
+    console.log(response.data);
+    for(let i = 0; i < response.data.length; i++){
+        if (response.data[i].movie.titleid === relevantResult){
+            return (1/(i+1));
+        }
+    }
+    return 0;
 }
 
-evaluateResult({},{})
+async function evaluateSingleResults(){
+    testData = [
+        {query: "batman begins", result: "tt0372784"},
+        {query: "eternal sunshine of the spotless mind", result: "tt0338013"},
+        {query: "attack of the killer manatee", result: "tt1979186"}
+    ]
+
+    result = 0;
+    for (let record of testData){
+        newresult = await evaluateResultSingleInput(record.query, record.result);
+        result += newresult;
+    }
+    result = result/testData.length;
+    return result;
+}
+
+
+async function main(){
+    result = await evaluateSingleResults();
+    console.log(result);
+}
+
+main();
+
+
+
