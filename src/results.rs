ae5e3fbd4d804f241ba4838a7fc491d995c71d86
Bobeshwar <ampanrane@gmail.com>
2023-12-08 21:27:30 -0500

    checked for null

--- src/results.rs
+++ src/results.rs
@@ -39,7 +39,9 @@ impl Scores{
         }
         for i in 0..k{
             if let Some(matches) =  self.documents.remove(&list[i].0){
-                output.push(ApiResult{ movie: movie_data.get_movie_details(list[i].0.clone()).unwrap() ,fields: (matches).into_iter().collect()});
+                if let Some(movie) = movie_data.get_movie_details(list[i].0.clone()){
+                    output.push(ApiResult{ movie: movie ,fields: (matches).into_iter().collect()});
+                }
             }
             
         }
@@ -68,12 +70,15 @@ impl Scores{
 
     pub fn rerank(&mut self, genres: Vec<String>, movie_data: Arc<MovieData>){
         for (document, _scores) in self.documents.iter(){
-            for genre in movie_data.get_movie_details(document.to_owned()).unwrap().genres.split(","){
-                for match_genre in genres.iter(){
-                    if genre == match_genre{
-                        if let Some(score) = self.term_scores.get(document){
-                            self.term_scores.insert(document.to_owned(),*score + 10.0 );
-                        }   
+            println!("{}", document);
+            if let Some(movie) = movie_data.get_movie_details(document.to_owned()){
+                for genre in movie.genres.split(","){
+                    for match_genre in genres.iter(){
+                        if genre == match_genre{
+                            if let Some(score) = self.term_scores.get(document){
+                                self.term_scores.insert(document.to_owned(),*score + 10.0 );
+                            }   
+                        }
                     }
                 }
             }
